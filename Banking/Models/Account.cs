a2adc92be658d53e933e95645531cbe787617b23
inderdeol <Inder94638@gmail.com>
2020-10-27 15:01:28 +0300

    Add project files.

---
+++ Banking/Models/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Banking.Models
+{
+    public class Account
+    {
+        [Key]
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+        public string AccountNumber { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
